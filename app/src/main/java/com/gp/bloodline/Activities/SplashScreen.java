9d0ed441deb74ccb9aefba91a3d61c3800487ef5
alhuri <46427957+alhuri@users.noreply.github.com>
2021-05-05 12:44:28 +0300

    final
    final

---
+++ app/src/main/java/com/gp/bloodline/Activities/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.gp.bloodline.Activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.gp.bloodline.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash_screen);
+    Handler handler = new Handler();
+    handler.postDelayed(new Runnable() {
+      @Override
+      public void run() {
+        startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+      }
+    }, 1500);
+  }
+}
